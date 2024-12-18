342fe4e026b89d25bb063f4213c4f4d53f5dd377
oreopk <sart6zav@yandex.ru>
2024-12-18 11:39:07 +0300

    refactor: fix selector to rule 3.1 and 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -42,11 +42,11 @@ var createNewTaskElement=function(taskString){
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
-
+    editButton.classList.add('btn');
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
-
+    deleteButton.classList.add('btn');
 
     //and appending.
     listItem.appendChild(checkBox);
@@ -64,7 +64,7 @@ var addTask=function(){
     //Create a new list item with the text from the #new-task:
     if (!taskInput.value) return;
     var listItem=createNewTaskElement(taskInput.value);
-
+    listItem.classList.add('li');
     //Append listItem to incompleteTaskHolder
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskCompleted);
